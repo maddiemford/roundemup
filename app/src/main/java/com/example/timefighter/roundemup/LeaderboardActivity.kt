32f583d17038957b7851a1420aa33bae339ea204
Maddie Ford <maddiemford@gmail.com>
2020-08-15 15:40:16 -0400

    Add leaderboard activity

---
+++ app/src/main/java/com/example/timefighter/roundemup/LeaderboardActivity.kt
@@ -0,0 +1,12 @@
+package com.example.timefighter.roundemup
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LeaderboardActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_leaderboard)
+    }
+}
